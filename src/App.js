ca79a5ed6448e2bd980a44efcc52a743ad442c8f
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 03:45:17 +0000

    Fixed line 3 typo where function had BoardCreate not playerBoardCreate

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './Board.css';
-import { BoardCreate } from './Board.js';
+import { playerBoardCreate } from './Board.js';
 import React, { useEffect, useState, useRef } from 'react';
 
 function App() {
