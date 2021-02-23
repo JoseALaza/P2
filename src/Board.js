c563b210cdbb7b01573f2a53283e1955f4e92349
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 04:06:21 +0000

    Added key states that will determine player turns and the board.

--- src/Board.js
+++ src/Board.js
@@ -4,10 +4,25 @@ import io from 'socket.io-client';
 
 const socket = io();
 
+// Serves as a blank board that will only be used once
+const boardBase = ['', '', '', '', '', '', '', '', ''];
+
+// Creates a boardState that can be used to rerender when onClick is triggered
+const [board, setBoard] = useState(boardBase);
+
+// Creates a player state to determine who's turn it is.(X is default player1)
+// Still needs work as to determine if the player is allowed to go
+const [player, setPlayer] = useState('X')
+
 export function playerBoardCreate() {
-    
+
     return (
         <div>
+            <h1>{status}</h1>
+            <div class = "board" >
+                { board.map((item, val) => <Square key={val} idx={val} val={item} onClick={onClickSquare}/>) }
+            </div>
         </div>
+
     );
 }
