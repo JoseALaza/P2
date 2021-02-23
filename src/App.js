9c34ae0e9991243a37ecce9563c9cb331b9f8e17
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 00:01:14 +0000

    Basic login screen with button and username input

--- src/App.js
+++ src/App.js
@@ -7,21 +7,10 @@ const socket = io();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Login page</h1>
+      <input type="text" required/>
+      <button>Send</button>
     </div>
   );
 }
