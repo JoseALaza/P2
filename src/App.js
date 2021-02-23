c39d0c8af4ce8655457d4d4ab2664a559d1cf381
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 01:09:19 +0000

    Added conditional rendering in order to achieve a switch in username input and login

--- src/App.js
+++ src/App.js
@@ -4,35 +4,65 @@ import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 
 const socket = io();
-let  username;
+let username;
 
 function App() {
-  
+
+  // Serves to control the username of the current user. (could possibly use this as login state)
   const [user, setUser] = useState('')
-  
-  const usernameInput = useRef(null);
-  
+
+  // 0 refers to the current user not logged in, 1 is the user already logged in
+  const [loginState, setLogin] = useState(0)
+
+
+  const usernameInput = useRef(null); // Serves to extract username textbox
+
+  // Button function for Login sending username to the current state.
+  // Sets loginState to render Logout Page
   function onClickLogin() {
-    username = usernameInput.current.value;
-    
-    setUser(username);
+    if (usernameInput != null) {
+      username = usernameInput.current.value;
+      
+      setLogin(1);
+      setUser(username);
+    }
+  }
+
+  // Button functino for Logout removing username from state.
+  // Resets loginState to render to Login Page
+  function onClickLogout() {
+
+    setLogin(0);
+    setUser('');
   }
   
+  // Renders a login page to users by default
+  if (!loginState) {
+    return (
+      <div>
+        <h1>Login page</h1>
+        <label>Username:</label>
+        <input type="text" ref={usernameInput} required/>
+        <button onClick={onClickLogin}>Send</button>
+      </div>
+    );
+  }
   
-  
-  return (
-    <div>
-      <h1>Login page - {user}</h1>
-      <input type="text" required/>
-      <button onClick={onClickLogin}>Send</button>
-    </div>
-  );
-  
-  
-  
-  
-  
-  
+  // Renders a logout page once a user has submitted a valid username
+  if (loginState) {
+    return (
+      <div>
+        <h1>Logout page - {user}</h1>
+        <button onClick={onClickLogout}>Logout</button>
+      </div>
+    );
+  }
+
+
+
+
+
+
 }
 
 export default App;
