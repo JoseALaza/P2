fbc122cdd01fc8820a945bc85c23a0c398da252a
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 00:32:06 +0000

    Added username onClick funciton that updates the state of the page.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,35 @@ import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 
 const socket = io();
+let  username;
 
 function App() {
+  
+  const [user, setUser] = useState('')
+  
+  const usernameInput = useRef(null);
+  
+  function onClickLogin() {
+    username = usernameInput.current.value;
+    
+    setUser(username);
+  }
+  
+  
+  
   return (
     <div>
-      <h1>Login page</h1>
+      <h1>Login page - {user}</h1>
       <input type="text" required/>
-      <button>Send</button>
+      <button onClick={onClickLogin}>Send</button>
     </div>
   );
+  
+  
+  
+  
+  
+  
 }
 
 export default App;
