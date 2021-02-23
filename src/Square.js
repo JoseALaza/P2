acd5473c2a871d6ff245e77da31f5dc994bee841
JoseALaza <ec2-user@ip-172-31-57-106.ec2.internal>
2021-02-23 03:44:16 +0000

    Added Square.js that corresponds to a single Tic Tac Toe tile with values passed down from Board.js

---
+++ src/Square.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'; // Most likely not needed
+
+// Utilizes props that are passed down to determine index and onClick function
+export function Square(props) {
+    
+    // Renders a single square with index property at id, the passed down onClick function, and the associated value
+    return ( 
+        <div className="box" id={props.idx} onClick={props.onClick}>
+            {props.val}
+        </div>
+    );
+}
